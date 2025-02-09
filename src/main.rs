e9b55dcfee7e9e45368424afc2724e5b18fb182e
Ryan <etrigan976@gmail.com>
2025-02-09 13:49:09 -0800

    wrote first temp test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bbow::Bbow;
 fn main() {}
 #[test]
 fn extend_from_text_test() -> () {
+    use bbow::Bbow;
     let bbow = Bbow::new().extend_from_text("Hello world.");
     assert_eq!(2, bbow.len());
     assert_eq!(1, bbow.match_count("hello"));
